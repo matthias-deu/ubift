40be864492e6d59378feffcd6d92f0f70bbc9fef
Matthias <matthias_de@gmx.net>
2023-06-11 17:59:59 +0200

    updated outdated comment

--- ubift/exception.py
+++ ubift/exception.py
@@ -1,5 +1,5 @@
 """Contains all exceptions that the UBIFT application can throw."""
 
 class UBIFTException(Exception):
-    """The most general exception, all other exceptions inherit the IkitaException"""
+    """The most general exception, all other exceptions inherit the UBIFTException"""
 
